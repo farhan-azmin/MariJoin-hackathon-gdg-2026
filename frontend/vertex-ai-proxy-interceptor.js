5109261f560198a5dd100dfaf15490d67a86ad9a
Farhan Azmin <72971358+farhan-azmin@users.noreply.github.com>
2026-05-17 05:23:25 +0800

    icon update

--- frontend/vertex-ai-proxy-interceptor.js
+++ frontend/vertex-ai-proxy-interceptor.js
@@ -113,7 +113,7 @@
           headers: {
             'Content-Type': 'application/json',
             // Add a random header to identify these proxied requests on the Node.js backend.
-            'X-App-Proxy': 'yGhrb-xmeqSmtWbtWs_5jQ4pQlBeWaFt',
+            'X-App-Proxy': 'LnQtr17PAZ7-LEpGumNf-l4u8JmVeTc2',
           },
           body: JSON.stringify(requestDetails),
         };
